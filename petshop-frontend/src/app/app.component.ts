c4e7faf003d32c75efe39d8b5988111e32eb8a91
Andre <andrereginatto93@gmail.com>
2025-09-24 13:16:45 -0300

    Add primary input component

--- petshop-frontend/src/app/app.component.ts
+++ petshop-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimaryInputComponent } from "./components/primary-input/primary-input.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrimaryInputComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'petshop-frontend';
