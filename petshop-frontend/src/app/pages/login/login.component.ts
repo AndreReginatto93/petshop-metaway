1bbb80d3e87767f6d4dbb389de83d6e05c98aaeb
Andre <andrereginatto93@gmail.com>
2025-09-26 14:46:40 -0300

    Implement login functionality with service integration

--- petshop-frontend/src/app/pages/login/login.component.ts
+++ petshop-frontend/src/app/pages/login/login.component.ts
@@ -1,33 +1,60 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
   imports: [
     ReactiveFormsModule
   ],
+  providers:[
+    LoginService,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-    loginForm!: FormGroup;
+  loginForm!: FormGroup;
+  errorMessage: String = "";
+  loading: boolean = false;
 
   constructor(
+    private loginService: LoginService
   ){
     this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+      login: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
     }, { updateOn: 'submit' })
   }
 
   submit(){
-    if (this.loginForm.invalid) {
-      this.loginForm.markAllAsTouched(); // força exibir erros
-      return;
-    }
+    try{
+      //if (this.loading == true) return;
+      this.loading = true;
+      this.errorMessage = "";
+      if (this.loginForm.invalid) {
+        this.loginForm.markAllAsTouched(); // força exibir erros
+        console.log("markAllAsTouched");
+        return;
+      }
 
-    const { email, password } = this.loginForm.value;
-    alert(`Login com sucesso: ${email}`);
+      const { login, password } = this.loginForm.value;
+      console.log('user: %s, passsword: %s', login, password);
+
+      this.loginService.login(login, password).subscribe({
+        next: (res) => {
+          console.log('Login ok!', res);
+          localStorage.setItem('token', res.token); // exemplo
+          alert('Login realizado com sucesso!');
+        },
+        error: (err) => {
+          console.error(err);
+          this.errorMessage = 'Usuário ou senha inválidos';
+        }
+      });
+    }finally{
+      this.loading = false;
+    }
   }
 
   navigate(){
---
+++ petshop-frontend/src/app/services/login.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginResponse } from '../types/login-response';
+import { tap } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  private apiUrl = 'http://localhost:8080/api/v1/auth/login';
+  
+  constructor(private httpClient: HttpClient) { }
+
+  login(login: string, password: string){
+    console.log(this.apiUrl)
+
+    const json = {
+        login: login,
+        password: password
+    }
+
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.httpClient.post<LoginResponse>(this.apiUrl, json, { headers });
+  }
+}
